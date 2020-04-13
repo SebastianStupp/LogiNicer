b686871b4294e7ebecec526f3b4e1b7e66973bab
SebastianStupp <s.stupp@outlook.de>
2020-04-13 17:17:51 +0200

    Add example dropdown component and add it to storybook

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const Dropdown = styled.select`
+  background-color: #fff6d6;
+  border-radius: 3px;
+  min-width: 254px;
+  min-height: 34px;
+`;
+
+export default function DefaultDropdown() {
+  const [dropdownValue, setdropdownValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setdropdownValue(event.target.value);
+  };
+
+  return (
+    <Dropdown value={dropdownValue} onChange={handleChange}>
+      <option value="" selected disabled hidden>
+        Choose Example
+      </option>
+      <option value="Example One">Example One</option>
+      <option value="Example Two">Example Two</option>
+      <option value="Example Three">Example Three</option>
+      <option value="Example Four">Example Four</option>
+    </Dropdown>
+  );
+}

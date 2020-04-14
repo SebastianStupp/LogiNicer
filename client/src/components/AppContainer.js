f18ecfd855eda81c5d908567ce173483c705d652
SebastianStupp <s.stupp@outlook.de>
2020-04-14 16:16:27 +0200

    Remove background color

--- client/src/components/AppContainer.js
+++ client/src/components/AppContainer.js
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(#0d3955 60%, black);
   display: flex;
   flex-direction: column;
 `;

a62b0e1e28f6e269c2cf014730ba2ef6152b3a5e
SebastianStupp <s.stupp@outlook.de>
2020-04-14 16:11:45 +0200

    Add theming for storybook

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    basic: 'linear-gradient(#0d3955 60%, black);',
+    textprimary: 'linear-gradient(#0d3955 60%, black)',
+    textsecondary: '#fff6d6',
+    primary: '#db8f00',
+    secondary: '#fff6d6',
+    tertiary: '#707070',
+  },
+};
+
+export default theme;

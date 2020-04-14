346b1d43cdd79761bb643bed0fec22e5d7f94055
SebastianStupp <s.stupp@outlook.de>
2020-04-14 16:40:03 +0200

    Install and config npm storybook viewport

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,13 @@
 import { configure, addDecorator } from '@storybook/react';
 import themeDecorator from './themeDecorator';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 addDecorator(themeDecorator);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone6',
+  },
+});

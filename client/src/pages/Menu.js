cc449ef533325760c93578204eb017b36b7fbc6f
SebastianStupp <s.stupp@outlook.de>
2020-04-16 17:56:49 +0200

    Add flexible color change for menubutton and add array maping to create menubuttons

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import Header from '../components/Header';
-import { MenuButton, MenuButtonTransparent } from '../components/MenuButtons';
+import MenuButton from '../components/MenuButtons';
 
 const MenuContainer = styled.div`
   width: 100vw;
@@ -9,18 +9,32 @@ const MenuContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  & button:nth-of-type(even) {
+    background-color: rgb(219, 143, 0, 63%);
+  }
 `;
+
+const menuOptions = [
+  'Client Master',
+  'ItemMaster',
+  'Storage System',
+  'Inbound',
+  'Outbound',
+];
+
+const CreateMenuOptions = () => {
+  return menuOptions.map((menuOptions) => (
+    <MenuButton key={menuOptions}>{menuOptions}</MenuButton>
+  ));
+};
 
 export default function DefaultMenu() {
   return (
     <>
       <Header type="logout"></Header>
       <MenuContainer>
-        <MenuButton>Client Master</MenuButton>
-        <MenuButtonTransparent>Item Master</MenuButtonTransparent>
-        <MenuButton>Storage System</MenuButton>
-        <MenuButtonTransparent>Inbound</MenuButtonTransparent>
-        <MenuButton>Outbound</MenuButton>
+        <CreateMenuOptions></CreateMenuOptions>
       </MenuContainer>
     </>
   );

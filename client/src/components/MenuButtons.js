5e9f154331c9625dcd3355aa06db7da12e730188
SebastianStupp <s.stupp@outlook.de>
2020-04-16 17:55:13 +0200

    Remove transparentmenubutton

--- client/src/components/MenuButtons.js
+++ client/src/components/MenuButtons.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
-export const MenuButton = styled.button`
-  background-color: rgb(219, 143, 0);
+const MenuButton = styled.button`
+  background-color: ${(props) => props.theme.colors.primary};
   width: 100%;
   max-width: 500px;
   height: 30%;
@@ -9,17 +9,6 @@ export const MenuButton = styled.button`
   color: ${(props) => props.theme.colors.textprimary};
   padding: 0px;
   border: 0;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
 `;
 
-export const MenuButtonTransparent = styled.button`
-  background-color: rgb(219, 143, 0, 63%);
-  width: 100%;
-  max-width: 500px;
-  height: 30%;
-  font-size: 2rem;
-  color: ${(props) => props.theme.colors.textprimary};
-  padding: 0px;
-  border: 0;
-`;
+export default MenuButton;
--- client/src/stories/MenuBottons.stories.js
+++ client/src/stories/MenuBottons.stories.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import { MenuButton, MenuButtonTransparent } from '../components/MenuButtons';
+import MenuButton from '../components/MenuButtons';
 
 export default {
   title: 'Button',
 };
 
 export const DefaultMenuButton = () => <MenuButton>Default</MenuButton>;
-export const TransparentMenuButton = () => (
-  <MenuButtonTransparent>Transparent</MenuButtonTransparent>
-);

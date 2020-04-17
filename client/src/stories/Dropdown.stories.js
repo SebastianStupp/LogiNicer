490fd684a16b1a0277b16878d583dea9a7faeb09
SebastianStupp <s.stupp@outlook.de>
2020-04-17 14:20:30 +0200

    Add styled dropdown

---
+++ client/src/stories/Dropdown.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Dropdown from '../components/Dropdown';
+
+export default {
+  title: 'Dropdown',
+};
+
+export const DefaultDropdown = () => <Dropdown></Dropdown>;

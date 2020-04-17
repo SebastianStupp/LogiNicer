e7b15235e1ff3eb33bd62effad7e381f97933b8b
SebastianStupp <s.stupp@outlook.de>
2020-04-17 12:35:27 +0200

    Add styled input

---
+++ client/src/stories/Input.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Input from '../components/Input';
+
+export default {
+  title: 'Input',
+};
+
+export const DefaultInput = () => <Input onChange={action('Change')}></Input>;

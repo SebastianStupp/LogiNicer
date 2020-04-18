2373a28684f498d67c43d26558e269a65b289c64
SebastianStupp <s.stupp@outlook.de>
2020-04-18 13:13:22 +0200

    Add menu page

---
+++ client/src/pages/Menu.js
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import Header from '../components/Header';
+import MenuButton from '../components/MenuButton';
+
+const MenuContainer = styled.div`
+  width: 100vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & button:nth-of-type(even) {
+    background-color: rgb(219, 143, 0, 63%);
+  }
+`;
+
+const menuOptions = [
+  'Client Master',
+  'ItemMaster',
+  'Storage System',
+  'Inbound',
+  'Outbound',
+];
+
+const CreateMenuOptions = () => {
+  return menuOptions.map((menuOptions) => (
+    <MenuButton key={menuOptions}>{menuOptions}</MenuButton>
+  ));
+};
+
+export default function DefaultMenu() {
+  return (
+    <>
+      <Header type="logout"></Header>
+      <MenuContainer>
+        <CreateMenuOptions></CreateMenuOptions>
+      </MenuContainer>
+    </>
+  );
+}

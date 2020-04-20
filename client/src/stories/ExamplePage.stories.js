c550014d3883c8d8ab6198df0aac099ece7637d0
SebastianStupp <s.stupp@outlook.de>
2020-04-20 14:39:27 +0200

    Add styled example page

---
+++ client/src/stories/ExamplePage.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ExamplePage from '../pages/ExamplePage';
+
+export default {
+  title: 'Pages',
+};
+
+export const DefaultExamplePage = () => <ExamplePage></ExamplePage>;

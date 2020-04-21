279c1a9764032fd8ada3d6ba0a206ec450842cbb
SebastianStupp <s.stupp@outlook.de>
2020-04-21 13:28:43 +0200

    Add global styles and theme provider to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+import theme from './theme';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
-  return <div>Under Construction</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles></GlobalStyles>
+    </ThemeProvider>
+  );
 }
 
 export default App;

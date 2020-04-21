ef5864806b543f1b6d9abeea8b5fc0e33c191f14
SebastianStupp <s.stupp@outlook.de>
2020-04-21 14:03:55 +0200

    Add browserrouter and add route to login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import theme from './theme';
 import GlobalStyles from './GlobalStyles';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles></GlobalStyles>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }

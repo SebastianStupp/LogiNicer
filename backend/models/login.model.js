80f4825dfe60e12efe01ba12a255efc1ea39411a
SebastianStupp <s.stupp@outlook.de>
2020-04-22 13:54:21 +0200

    Create login schema

---
+++ backend/models/login.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const loginSchema = new Schema({
+  username: String,
+  password: String,
+});
+
+const Login = mongoose.model('Login', loginSchema);
+
+module.exports = Login;

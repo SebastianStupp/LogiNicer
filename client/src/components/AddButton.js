0ff50fa488de616b34634ecbfb29c9ca21188656
SebastianStupp <s.stupp@outlook.de>
2020-04-28 09:28:12 +0200

    Add pointer and onclick property

--- client/src/components/AddButton.js
+++ client/src/components/AddButton.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Add from '../assets/add.svg';
+import PropTypes from 'prop-types';
 
 const AddButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-bottom: 15px;
+  cursor: pointer;
 `;
 
-export default function AddButton() {
+export default function AddButton({ modal }) {
   return (
-    <AddButtonContainer>
+    <AddButtonContainer onClick={modal}>
       <img src={Add} alt="A Plus Symbol"></img>
     </AddButtonContainer>
   );
 }
+
+AddButton.propTypes = {
+  modal: PropTypes.func,
+};

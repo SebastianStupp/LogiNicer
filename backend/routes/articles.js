80421b0d7991e493c5039f092982bf2da690adbe
SebastianStupp <s.stupp@outlook.de>
2020-05-05 18:09:38 +0200

    Add delete/patch article routes

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -8,15 +8,15 @@ router.get('/', (request, response) => {
 });
 
 router.post('/', (request, response) => {
-  const articleNumber = request.body.articlenumber;
-  const client = request.body.clientname;
-  const bbd = request.body.bdd;
+  const articlenumber = request.body.articlenumber;
+  const client = request.body.client;
+  const bbd = request.body.bbd;
   const pzn = request.body.pzn;
   const ean = request.body.ean;
   const time = request.body.time;
 
   const newArticle = new Article({
-    articleNumber,
+    articlenumber,
     client,
     bbd,
     pzn,
@@ -26,6 +26,30 @@ router.post('/', (request, response) => {
 
   newArticle
     .save()
+    .then((newArticle) => response.json(newArticle))
+    .catch((err) => response.json('Error:' + err));
+});
+
+router.patch('/:id', (request, response) => {
+  Article.findByIdAndUpdate(
+    { _id: request.params.id },
+    {
+      $set: {
+        articlenumber: request.body.articlenumber,
+        client: request.body.client,
+        bbd: request.body.bbd,
+        pzn: request.body.pzn,
+        ean: request.body.ean,
+      },
+    },
+    { new: true }
+  )
+    .then((clientname) => response.json(clientname))
+    .catch((err) => response.json('Error:' + err));
+});
+
+router.delete('/:id', (request, response) => {
+  Article.findByIdAndRemove({ _id: request.params.id })
     .then((article) => response.json(article))
     .catch((err) => response.json('Error:' + err));
 });

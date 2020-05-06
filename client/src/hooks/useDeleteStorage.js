2e69541626b26d0f679a47c3b5d726439522835f
SebastianStupp <s.stupp@outlook.de>
2020-05-06 18:18:14 +0200

    Change hook props

--- client/src/hooks/useDeleteStorage.js
+++ client/src/hooks/useDeleteStorage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { deleteStorage } from '../api/storages';
 
-export default function useDeleteStorage(storageId) {
+export default function useDeleteStorage() {
   const [deletedStorage, setDeletedStorage] = React.useState(null);
   const [errorStorage, setErrorStorage] = React.useState(false);
   const [loadingStorage, setLoadingStorage] = React.useState(true);
 
-  async function doDeleteStorage() {
+  async function doDeleteStorage(storageId) {
     try {
       setLoadingStorage(true);
       const deletedStorage = await deleteStorage(storageId);

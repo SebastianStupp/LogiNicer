21c982162938f894260def8ad1e514a87ea3942b
SebastianStupp <s.stupp@outlook.de>
2020-05-06 20:45:15 +0200

    Add dropdown component for article and storage

---
+++ client/src/components/StorageDropdown.js
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import useGetStorages from '../hooks/useGetStorages';
+import PropTypes from 'prop-types';
+
+const DropdownSelection = styled.select`
+  background-color: ${(props) => props.theme.colors.secondary};
+  border-radius: 3px;
+  min-width: 254px;
+  min-height: 45px;
+  font-size: 1.3rem;
+  text-align: center;
+  margin-bottom: 10px;
+  text-align-last: center;
+`;
+
+export default function StorageDropdown({ optionTitle, onContentChange }) {
+  const [dropdownValue, setdropdownValue] = React.useState('');
+  const [{ storages, loading, error }] = useGetStorages();
+
+  React.useEffect(() => {
+    if (dropdownValue) {
+      onContentChange(dropdownValue);
+    }
+  }, [dropdownValue]);
+
+  const handleChange = (event) => {
+    setdropdownValue(event.target.value);
+  };
+
+  return (
+    <DropdownSelection value={dropdownValue} onChange={handleChange}>
+      <option value="" defaultValue disabled>
+        {optionTitle}
+      </option>
+      {loading && 'loading...'}
+      {error && 'Fehler'}
+      {storages &&
+        storages.map((data) => (
+          <option value={data.storage} key={data._id}>
+            {data.storage}
+          </option>
+        ))}
+      ;
+    </DropdownSelection>
+  );
+}
+StorageDropdown.propTypes = {
+  optionTitle: PropTypes.string,
+  onContentChange: PropTypes.func,
+};

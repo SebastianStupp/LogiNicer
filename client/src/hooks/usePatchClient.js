939345df5aa26d778d2f6efe86f69a704744bf38
SebastianStupp <s.stupp@outlook.de>
2020-05-06 16:07:48 +0200

    Refactor Naming

--- client/src/hooks/usePatchClient.js
+++ client/src/hooks/usePatchClient.js
@@ -3,19 +3,19 @@ import { patchClient } from '../api/clients';
 
 export default function usePatchClient() {
   const [clientName, setClientName] = React.useState(null);
-  const [errormsg, setError] = React.useState(false);
+  const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
 
   async function doPatchClient(clientId, clientname) {
     try {
       const clientName = await patchClient(clientId, clientname);
       setClientName(clientName);
-    } catch (errormsg) {
+    } catch (error) {
       setError(true);
     } finally {
       setLoading(false);
     }
   }
 
-  return [{ clientName, errormsg, loading }, doPatchClient];
+  return [{ clientName, error, loading }, doPatchClient];
 }

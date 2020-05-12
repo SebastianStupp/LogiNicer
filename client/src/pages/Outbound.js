b2c11b2661840afa5fbcf22e5d39bd97ec9293c4
SebastianStupp <s.stupp@outlook.de>
2020-05-12 12:25:37 +0200

    Remove state

--- client/src/pages/Outbound.js
+++ client/src/pages/Outbound.js
@@ -98,7 +98,6 @@ export default function Outbound() {
   function handleOnClickOutbound() {
     setAmount(amount - outboundAmount);
     setConfirm(true);
-    setConfirm(false);
   }
 
   return (

3742906a9366796d19cc808a6ac739fbbfe1367c
SebastianStupp <s.stupp@outlook.de>
2020-05-14 14:13:46 +0200

    Add storybook react router

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,9 +2,10 @@ import { addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { configure, addDecorator } from '@storybook/react';
 import themeDecorator from './themeDecorator';
+import StoryRouter from 'storybook-react-router';
 
 addDecorator(themeDecorator);
-
+addDecorator(StoryRouter());
 addParameters({
   viewport: {
     viewports: INITIAL_VIEWPORTS,
